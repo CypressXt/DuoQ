274f04869b45f2b518faa0428f3539d79b9b03fa
CypressXt <cypress@cypressxt.net>
2015-01-25 22:09:07 +0100

    update register's form validation

--- app/models/app_user.rb
+++ app/models/app_user.rb
@@ -1,7 +1,7 @@
 class AppUser < ActiveRecord::Base
 	attr_accessor :password_confirmation
 
-	validates :username, :email, :password, presence: true
+	validates :username, :email, :password, :password_confirmation, presence: true
 	validates :password, confirmation: true
 	validates :username, length: { in: 4..20 }
 	validates :email, :username, uniqueness: true
