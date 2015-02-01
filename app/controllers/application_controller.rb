930a839d5298ad95936f09b7d00891e67d081cd1
CypressXt <cypress@cypressxt.net>
2015-02-01 16:13:40 +0100

    update the root url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+  if Rails.env.production?
+  	{:host => "duoq.cypressxt.net"}
+  end
 end
