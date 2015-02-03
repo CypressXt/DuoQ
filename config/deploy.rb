3f8dd8784387d3920204e264741ecb94a328e5f7
CypressXt <cypress@cypressxt.net>
2015-02-03 15:14:59 +0100

    update capistrano deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,14 +34,17 @@ set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-	namespace :deploy do
+after "deploy", "deploy:restart" 
 
-		after :restart, :clear_cache do
-			on roles(:web), in: :groups, limit: 3, wait: 10 do
+namespace :deploy do
+
+	after :restart, :clear_cache do
+		on roles(:web), in: :groups, limit: 3, wait: 10 do
 	      # Here we can do anything such as:
 	      # within release_path do
 	      #   execute :rake, 'cache:clear'
 	      # end
+	      execute "/home/cypress/restartPuma.sh"
 	  end
 	end
 end
