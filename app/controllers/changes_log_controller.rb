32611ce7511e67459d47f3be47aba8cfe361dc23
CypressXt <cypress@cypressxt.net>
2015-02-08 01:47:13 +0100

    Performances update for changelogs

--- app/controllers/changes_log_controller.rb
+++ app/controllers/changes_log_controller.rb
@@ -1,16 +1,11 @@
 class ChangesLogController < ApplicationController
 
 	def get_commits
-		nbCommits=10
 		github = Github.new login:Rails.application.secrets.github_user, password:Rails.application.secrets.github_password
 		@commits=[]
-		data = github.repos.commits.all 'CypressXt', 'DuoQ'
+		data = github.repos.commits.all 'CypressXt', 'DuoQ', per_page: 10
 		data.each do |commit|
 			@commits << commit.commit
-			nbCommits=nbCommits-1
-			if nbCommits == 0
-				break
-			end
 		end
 	end
 
