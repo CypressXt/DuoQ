7e4f2e6a68545b21687311784a215d303d3ee53d
CypressXt <cypress@cypressxt.net>
2015-05-10 19:02:01 +0200

    Updated index view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
 	def index
-
+		@nbMatches = Match.all.count
+		@nbPlayers =  Summoner.all.where(validated: true).count
+		@nbTeams = Team.all.count
 	end
 
 end
