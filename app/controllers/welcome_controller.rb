9e8a8280151eefc64f8bf16c2d3f2d746c5ad8c6
CypressXt <cypress@cypressxt.net>
2015-06-23 02:25:48 +0200

    Updated welcome page
     & solved bootstrap icons issues

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,14 @@ class WelcomeController < ApplicationController
 		@nbMatches = Match.all.count
 		@nbPlayers =  Summoner.all.where(validated: true).count
 		@nbTeams = Team.all.count
+		@nbRiotRequest = 0
+		@nbDelayedRiotRequest = 0
+		@nb404RiotRequest = 0
+		if File.exist?("log/riot_api.log")
+			@nbRiotRequest = open("log/riot_api.log").grep(/RiotRequest/).count
+			@nbDelayedRiotRequest = open("log/riot_api.log").grep(/429/).count
+			@nb404RiotRequest = open("log/riot_api.log").grep(/Not Found/).count
+		end
 	end
 
 end
